c7b046bd6a302d324153dd5c053b7fb4684849d6
Ari He <38049857+ariiscoding@users.noreply.github.com>
2025-06-24 21:43:59 -0700

    Package

--- Package.swift
+++ Package.swift
@@ -5,10 +5,18 @@ import PackageDescription
 
 let package = Package(
     name: "DiscordTranslationBot",
+    dependencies: [
+        .package(url: "https://github.com/SwiftcordApp/DiscordKit", branch: "main")
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .executableTarget(
-            name: "DiscordTranslationBot"),
+            name: "DiscordTranslationBot",
+            dependencies: [
+                .product(name: "DiscordKitBot", package: "DiscordKit")
+            ],
+            path: "Sources",
+        ),
     ]
 )
